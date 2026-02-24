b9b0257de20b53e87d83131d28df5ab3a5945990
Octavian Popescu <popescu.octavian09@gmail.com>
2026-02-24 12:52:58 +0000

    compiles, 4 tests failing

---
+++ hardhat.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.26",
+        settings: {
+          evmVersion: "cancun",
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 44444444,
+          },
+          metadata: {
+            bytecodeHash: "none",
+          },
+        },
+      },
+      debug: {
+        version: "0.8.26",
+        settings: {
+          viaIR: false,
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  paths: {
+    sources: "./src",
+    tests: {
+      solidity: "./test",
+    },
+  },
+
+  test: {
+    //! We don't have profiles for tests
+    solidity: {
+      ffi: true,
+      allowInternalExpectRevert: true,
+      fuzz: {
+        runs: 1000,
+        seed: "0x4444",
+      },
+      fsPermissions: {
+        //! snapshots not yet released in HH
+        // dangerouslyReadWriteDirectory: ["./snapshots"],
+        readDirectory: ["./out", "./test/bin"],
+      },
+    },
+  },
+});
